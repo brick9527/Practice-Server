530090d0aed637f92f6c3f00971cf286cc34ac9b
brick9527 <brick9527@foxmail.com>
2021-08-08 21:53:53 +0800

    feat: 添加运行接口

--- app/controller/code.js
+++ app/controller/code.js
@@ -2,12 +2,20 @@
 
 const Controller = require('egg').Controller;
 
+const wrapper = require('../libs/wrapper');
+
 class CodeController extends Controller {
   async run() {
     const { ctx } = this;
     const { body } = ctx.request;
+    const { code } = body;
 
-    ctx.body = 'hello ' + JSON.stringify(body);
+    // eslint-disable-next-line no-eval
+    const result = eval(wrapper(code));
+
+    console.log('result = ', result);
+
+    ctx.body = result;
   }
 }
 
